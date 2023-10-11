c41294d7d12ff625e732e25f37ef7cc29f69a8dc
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2023-10-11 22:55:35 +0300

    yak

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,25 +3,53 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-const galleryContainer = document.querySelector(".gallery");
-const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
-galleryContainer.addEventListener("click", onImgClick);
+const gallery = document.querySelector(".gallery");
+const body = document.querySelector("body");
 
-// rendered items
-function createGalleryItemsMarkup(items) {
-  return items
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
+const imgArr = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
-    />
-  </a>
-</div>`;
-    })
-    .join("");
+    /></a></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", imgArr);
+
+gallery.addEventListener("click", onPhotoClick);
+
+function onPhotoClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName === "IMG") {
+    const largePhotoLink = evt.target.dataset.source;
+    const instance = basicLightbox.create(
+      `
+		<img width="1400" height="900" src="${largePhotoLink}">
+	`,
+      {
+        onShow: () => {
+          document.addEventListener("keydown", closeModal);
+          body.classList.add("no-scroll");
+        },
+        onClose: () => {
+          document.removeEventListener("keydown", closeModal);
+          body.classList.remove("no-scroll");
+        },
+      }
+    );
+    instance.show();
+
+    function closeModal(evt) {
+      if (evt.code === "Escape") {
+        instance.close();
+      }
+    }
+  }
 }
